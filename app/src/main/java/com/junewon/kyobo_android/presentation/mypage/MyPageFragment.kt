fb6fb90b3c97f3d476f0e53b85c8204cbb2af37b
murjune <sis9221302@gmail.com>
2022-11-17 17:08:28 +0900

    #3 [feat] 바텀 네비게이션 화면 전환 로직 구현

---
+++ app/src/main/java/com/junewon/kyobo_android/presentation/mypage/MyPageFragment.kt
@@ -0,0 +1,18 @@
+package com.junewon.kyobo_android.presentation.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.junewon.kyobo_android.R
+
+class MyPageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    }
+}
