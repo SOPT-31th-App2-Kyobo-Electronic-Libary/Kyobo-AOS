fc6cfe7277ebe6bd5213be989b5d4cb65b5271c9
murjune <sis9221302@gmail.com>
2022-11-24 22:22:09 +0900

    #11 [add] RecentBook Entity 추가

---
+++ app/src/main/java/com/junewon/kyobo_android/data/entity/RecentBook.kt
@@ -0,0 +1,9 @@
+package com.junewon.kyobo_android.data.entity
+
+data class RecentBook(
+    val id: Int = -1,
+    val title: String = "단단한 심층강화학습",
+    val author: String = "멀티코어",
+    val image: String = "https://blog.kakaocdn.net/dn/dUCwpv/btrtb1YDnau/sjvLGYK99buo1vgbNnnKF1/img.png",
+    val publisher: String = "홍익대학교"
+)
