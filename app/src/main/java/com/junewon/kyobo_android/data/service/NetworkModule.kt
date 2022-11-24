4f4bf5acae3e479a72de7a99b924ecd360349b85
waterminn <tnals08019@gmail.com>
2022-11-24 20:09:30 +0900

    [feat] ApiFactory 구현

---
+++ app/src/main/java/com/junewon/kyobo_android/data/service/NetworkModule.kt
@@ -0,0 +1,18 @@
+package com.junewon.kyobo_android.data.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://3.35.184.0:3000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    inline fun <reified T> create(): T = retrofit.create(T::class.java)
+}
+
+object ServicePool {
+    val homeService = NetworkModule.create<HomeService>()
+}
