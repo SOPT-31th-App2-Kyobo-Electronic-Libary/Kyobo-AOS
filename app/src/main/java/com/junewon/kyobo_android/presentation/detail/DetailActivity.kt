a6800057ec11a0221879b36103ba9ecf11797fa5
이준원 <sis9221302@gmail.com>
2022-11-25 04:47:19 +0900

    [add] DetailActivity 추가

---
+++ app/src/main/java/com/junewon/kyobo_android/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.junewon.kyobo_android.presentation.detail
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class DetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { }
+    }
+}
